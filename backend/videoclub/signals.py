25e26493c7c11fdcd05eaebc8dbe8e1f65191323
duckraper <klyman047@gmail.com>
2024-02-24 23:36:21 -0500

    agregadas seniales para poblar bases de datos en debug

--- backend/videoclub/signals.py
+++ backend/videoclub/signals.py
@@ -1,8 +1,52 @@
 from django.db import migrations
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 from django.dispatch import receiver
 from django.apps import apps
 import json
+import codecs
+
+
+@receiver(post_migrate)
+def poblar_con_videoclubs(sender, **kwargs):
+    "Pobla la base de datos con videoclubs, a partir de utils.videoclubs.json"
+    APP_NAME = "videoclub"
+    if sender.name == APP_NAME:
+        VideoClub = apps.get_model(APP_NAME, "VideoClub")
+        Municipio = apps.get_model(APP_NAME, "Municipio")
+
+        with codecs.open('videoclub/utils/videoclubs.json', 'r', encoding='utf-8') as datos:
+            videoclubs = json.load(datos)
+
+        print("Poblando con videoclubs ...")
+        for videoclub in videoclubs:
+            m = Municipio.objects.get(nombre=videoclub["municipio"])
+            vc, vc_created = VideoClub.objects.get_or_create(
+                nombre=videoclub["nombre"],
+                municipio=m,
+                direccion=videoclub["direccion"],
+                telefono=videoclub["telefono"]
+            )
+            if vc_created:
+                print(vc)
+            vc.save()
+
+
+@receiver(post_migrate)
+def poblar_con_generos(sender, **kwargs):
+    "Pobla la base de datos con generos de peliculas, a partir de utils.generos.json"
+    APP_NAME = "videoclub"
+    if sender.name == APP_NAME:
+        Genero = apps.get_model(APP_NAME, "Genero")
+
+        with codecs.open('videoclub/utils/generos.json', 'r', encoding='utf-8') as datos:
+            generos = json.load(datos)
+
+        print("Poblando con generos ...")
+        for genero in generos:
+            g, g_created = Genero.objects.get_or_create(nombre=genero)
+            if g_created:
+                print(g)
+            g.save()
 
 
 @receiver(post_migrate)
@@ -15,9 +59,10 @@ def crear_registros_provincias_municipios(sender, **kwargs):
         Provincia = apps.get_model(APP_NAME, "Provincia")
         Municipio = apps.get_model(APP_NAME, "Municipio")
 
-        with open('videoclub/assets/provincias.json', 'r') as datos:
+        with codecs.open('videoclub/utils/provincias.json', 'r', encoding='utf-8') as datos:
             datos_provincias = json.load(datos)
 
+        print("Poblando con provincias y municipios ...")
         for provincia in datos_provincias:
             p, p_created = Provincia.objects.get_or_create(
                 nombre=provincia["nombre"])
