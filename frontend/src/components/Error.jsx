7ea10e2234cbd25b22d7ac0f2f807672fc4f1a4f
duckraper <klyman047@gmail.com>
2024-02-27 15:43:53 -0500

    restaurado el frontend a 0

--- frontend/src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = ({ children }) => {
-    return (
-        <div className="bg-red-500 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
-            {children}
-        </div>
-    );
-};
-
-export default Error;
